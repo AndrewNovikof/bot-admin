05858015291ce884aa6901e22b0c19cf388dd3c8
Andrew Novikov <anovikov@homecredit.ru>
2022-01-13 01:41:46 +0300

    show is enabled on the main table of the scheduleEvent

--- resources/js/Pages/Bot/ScheduleEvent/ScheduleEventList.vue
+++ resources/js/Pages/Bot/ScheduleEvent/ScheduleEventList.vue
@@ -22,6 +22,10 @@
                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Status
                             </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                              Enabled
+                            </th>
                             <th scope="col" class="relative px-6 py-3">
                                 <span class="sr-only">Edit</span>
                             </th>
@@ -44,6 +48,13 @@
                                       v-bind:class="{ 'bg-green-100 text-green-800': scheduleEvent.is_available, 'bg-red-100 text-red-800': !scheduleEvent.is_available }">
                                     {{ scheduleEvent.is_available ? 'Active' : 'Inactive'}}
                                   </span>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                    <span
+                                        class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full"
+                                        v-bind:class="{ 'bg-green-100 text-green-800': scheduleEvent.is_enabled, 'bg-red-100 text-red-800': !scheduleEvent.is_enabled }">
+                                      {{ scheduleEvent.is_enabled ? 'Enabled' : 'Disabled'}}
+                                    </span>
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                 <jet-button type="button" class="ml-2" @click="editScheduleEvent(scheduleEvent)">
